40ca8ea4b488d6adb620bfd6fb297cf13c71e706
mehedimohammad213 <mehedimohammad213@gmail.com>
2025-12-11 14:50:19 +0600

    fi x

--- frontend/src/components/MobileNavigation.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const MobileNavigation: React.FC = () => {
-  // Mobile navigation is now handled in the Header component
-  // This component is kept for backward compatibility but renders nothing
-  return null;
-};
-
-export default MobileNavigation;
