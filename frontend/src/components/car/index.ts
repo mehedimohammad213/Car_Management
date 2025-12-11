e8ceb24c70dbbec0e9da11258bc69043875911df
mehedimohammad213 <mehedimohammad213@gmail.com>
2025-12-11 14:51:51 +0600

    fix

--- frontend/src/components/car/index.ts
+++ frontend/src/components/car/index.ts
@@ -1,5 +1,4 @@
 // Car Components Exports
-export { default as CarForm } from "./CarForm";
 export { default as CarFormModal } from "./CarFormModal";
 export { default as CarTable } from "./CarTable";
 
